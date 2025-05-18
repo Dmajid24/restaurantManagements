b01473b7ca9a2c6907e394b755b450db59e4338c
Majidd <123636929+Dmajid24@users.noreply.github.com>
2025-05-19 04:40:04 +0700

    STAFF

--- app/Models/transactionHeader.php
+++ app/Models/transactionHeader.php
@@ -4,29 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class transactionHeader extends Model
+class TransactionHeader extends Model
 {
-    protected $table = 'transactionHeader';
-    protected $primaryKey = 'TransactionID';
-    public $incrementing = false; // Karena ID-nya bukan auto-increment
-    protected $keyType = 'string'; // Karena tipe datanya VARCHAR
-    public $timestamps = false; // Jika tabel tidak punya created_at dan updated_at
-
-    // Relasi ke customer
-    public function customer()
-    {
-        return $this->belongsTo(MsCustomer::class, 'customerID', 'customerID');
-    }
-
-    // Relasi ke staff
-    public function staff()
-    {
-        return $this->belongsTo(MsStaff::class, 'staffID', 'staffID');
-    }
-
-    // Relasi ke detail transaksi
+    protected $table = 'transactionheader';
+    protected $primaryKey = 'transactionID';
+    
     public function transactionDetails()
     {
-        return $this->hasMany(TransactionDetail::class, 'TransactionID', 'TransactionID');
+        return $this->hasMany(TransactionDetail::class, 'transactionID');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,40 @@
 <?php
 
-use App\Http\Controllers\viewOrder;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\HistoryController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route ke Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// Route ke ProductController
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.detail');
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+
+
+// Staff
+Route::prefix('staff')->group(function () {
+    Route::get('/', [StaffController::class, 'index'])->name('staff.index');
+    Route::get('/create', [StaffController::class, 'create'])->name('staff.create');
+    Route::post('/', [StaffController::class, 'store'])->name('staff.store');
+    Route::get('/{id}', [StaffController::class, 'show'])->name('staff.show');
+    Route::get('/{id}/edit', [StaffController::class, 'edit'])->name('staff.edit');
+    Route::put('/{id}', [StaffController::class, 'update'])->name('staff.update');
+    Route::delete('/{id}', [StaffController::class, 'destroy'])->name('staff.destroy');
+});
+
+
+// Order History
+Route::prefix('history')->group(function () {
+    Route::get('/', [HistoryController::class, 'index'])->name('history.index');
+    Route::delete('/{id}', [HistoryController::class, 'destroy'])->name('history.destroy');
+    // Tambahkan route show jika diperlukan
+    Route::get('/{id}', [HistoryController::class, 'show'])->name('history.show');
 });
